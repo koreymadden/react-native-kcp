b92976298be59e83882fe179382940e12b504d08
as3long <as3long@qq.com>
2022-08-01 12:10:20 +0800

    修改版本信息

--- examples/echo.ts
+++ examples/echo.ts
@@ -8,10 +8,6 @@ const host = '127.0.0.1';
 const port = 22333;
 const conv = 255;
 
-// fec 前向纠错
-const dataShards = 4;
-const parityShards = 1;
-
 // 加密
 const algorithm: crypto.CipherGCMTypes = 'aes-128-gcm';
 const key = crypto.randomBytes(128 / 8);
@@ -21,8 +17,6 @@ const iv = crypto.randomBytes(12);
 const listener = ListenWithOptions({
     port,
     block: new AesBlock(algorithm, key, iv),
-    dataShards,
-    parityShards,
     callback: (session) => {
         // accept new session
         session.on('recv', (buff: Buffer) => {
@@ -37,14 +31,12 @@ const session = DialWithOptions({
     port,
     host,
     block: new AesBlock(algorithm, key, iv),
-    dataShards,
-    parityShards,
 });
 session.on('recv', (buff: Buffer) => {
     log('recv:', buff.toString());
 });
 setInterval(() => {
-    const msg = Buffer.from(new Date().toISOString());
+    const msg = Buffer.from('hello world,' + new Date().toISOString());
     log(`send: ${msg}`);
     session.write(msg);
 }, 1000);

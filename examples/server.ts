5fb8786a52bf9a07f2e13e0656051e76a8d4e900
Korey Madden <maddenkorey@gmail.com>
2023-07-21 20:42:29 -0400

    updated examples and readme

--- examples/server.ts
+++ examples/server.ts
@@ -1,24 +1,17 @@
 //@ts-nocheck
 import dgram from 'react-native-udp';
-import { ListenWithOptions } from 'react-native-kcp';
-
-const port = 3333;
+import { Listen } from 'react-native-kcp';
 
 export const kcpServer = () => {
     const socketInstance = dgram.createSocket({ type: 'udp4' });
     socketInstance.bind(port);
-    const listener = ListenWithOptions(
-        {
-            port,
-            callback: (session) => {
-                session.on('recv', (buff: Buffer) => {
-                    const messageFromClient = buff.toString();
-                    console.debug('[MESSAGE RECEIVED FROM CLIENT]:', messageFromClient);
-                    console.debug('[SENDING MESSAGE BACK]');
-                    session.write(Buffer.from(`[GREETINGS FROM HOST]: ${messageFromClient}`));
-                });
-            },
-        },
-        socketInstance,
-    );
+
+    const listener = Listen(socketInstance, (session) => {
+        session.on('recv', (buff: Buffer) => {
+            const messageFromClient = buff.toString();
+            console.debug('[MESSAGE RECEIVED FROM CLIENT]:', messageFromClient);
+            console.debug('[SENDING MESSAGE BACK]');
+            session.write(Buffer.from(`[GREETINGS FROM HOST]: ${messageFromClient}`));
+        });
+    });
 };
